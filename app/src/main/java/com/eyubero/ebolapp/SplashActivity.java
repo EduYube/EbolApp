b7abfe19d909f5df02fb7673093c6ee42ac5cb59
eyubero <eyubero@vectoritcgroup.com>
2018-02-01 18:11:50 +0100

    launch main activity by thread

--- app/src/main/java/com/eyubero/ebolapp/SplashActivity.java
+++ app/src/main/java/com/eyubero/ebolapp/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.eyubero.ebolapp;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,8 +12,18 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 //        Start main activity on an simple mode
-        nextActivity();
+//        nextActivity();
 
+//        Launch main activity by thread
+//        We need to launch a thread runneable and catch it with a handler, because why want to launch after 3 sec
+        Runnable launch = new Runnable() {
+            @Override
+            public void run() {
+                nextActivity();
+            }
+        };
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(launch,3000);
     }
 
     public void nextActivity(){
